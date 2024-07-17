ecdff949a712905ba7dbf02f5737e582c1aedbf1
Dan-creates <124089519+Dan-creates@users.noreply.github.com>
2024-07-17 13:01:32 +0300

    Initialized Components

---
+++ src/Nav.js
@@ -0,0 +1,18 @@
+/**
+ * @returns A fully functional navigation bar.
+ */
+import './global.css'
+function NavBar(){
+    return(
+        <div id="NavBar" className='fl col'>
+            <div id="NavFunc">
+            <div id="Logo-container"></div>
+            <div id="Common-func"></div>
+            </div>
+            <div id="Search-Container">
+
+            </div>
+        </div>
+    );
+}
+export default NavBar;
