61b001b720a5ac1560eded52d326684ddc917ed2
Dan-creates <124089519+Dan-creates@users.noreply.github.com>
2024-08-06 20:16:01 +0300

    Shopping Cart  Implementation Test I

--- src/components/Cart-item.js
+++ src/components/Cart-item.js
@@ -1,3 +1,23 @@
 import '../styles/global.css'
+import Test_img from '../images/category.jpg'
 import Card from './Card'
 import Btn from './Buttons'
+function CartItem({name,price}){
+    return (
+    <Card stack='row'>
+        <div className='m-sq'>
+            <img  src={Test_img} alt=''/>
+        </div>
+        <Card stack='col'>
+            <h4>{name}</h4>
+            <h4>{price}</h4>
+        </Card>
+        <Card stack='row'>
+            <Btn custom text='-' className='s-sq'/>
+            <input type='number' placeholder='1' value={1} className='s-sq'/>
+            <Btn custom text='+' className='s-sq'/>
+        </Card>
+    </Card>
+    )
+}
+export default CartItem
